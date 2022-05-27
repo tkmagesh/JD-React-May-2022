0c88eb80312d267aadab92d3430fbd1d427e12be
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-27 12:35:28 +0530

    async intro

---
+++ 07-async/async-demos.js
@@ -0,0 +1,34 @@
+(function(){
+    function addSync(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        const result = x + y;
+        console.log(`   [@service] returning result`)
+        return result;
+    }
+
+    function addSyncClient(x,y){
+        console.log(`[@client] invoking the service`)
+        const result = addSync(x,y);
+        console.log(`[@client] result = ${result}`)
+    }
+
+    window['addSyncClient'] = addSyncClient;
+
+    function addAsync(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        setTimeout(function(){
+            const result = x + y;
+            console.log(`   [@service] returning result`)
+            return result;
+        }, 4000);
+    }
+
+    function addAsyncClient(x,y){
+        console.log(`[@client] invoking the service`)
+        const result = addAsync(x,y);
+        console.log(`[@client] result = ${result}`)
+    }
+
+    window['addAsyncClient'] = addAsyncClient;
+
+})();
